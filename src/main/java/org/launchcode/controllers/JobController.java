db5ec5640045009d0aa7ccaa821953cc3a92d4c9
Chris Ramlakan <ramlakan@gmail.com>
2019-04-16 14:31:07 -0400

    TODO 1 & 2 Completed

--- src/main/java/org/launchcode/controllers/JobController.java
+++ src/main/java/org/launchcode/controllers/JobController.java
@@ -1,12 +1,15 @@
 package org.launchcode.controllers;
 
+import org.launchcode.models.Job;
 import org.launchcode.models.forms.JobForm;
 import org.launchcode.models.data.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.validation.Valid;
 
@@ -20,11 +23,25 @@ public class JobController {
     private JobData jobData = JobData.getInstance();
 
     // The detail display for a given Job at URLs like /job?id=17
+//    @RequestMapping(value="edit/{cheeseID}", method = RequestMethod.GET)
+//    public String displayEditForm(Model model, @PathVariable int cheeseID){
+//        Cheese cheeseToEdit = CheeseData.getByID(cheeseID);
+//        if(cheeseToEdit == null) {
+//            return "redirect:";
+//        }
+//        model.addAttribute("title"," Edit Cheeses");
+//        model.addAttribute("cheese", cheeseToEdit);
+//        model.addAttribute("cheeseTypes", CheeseType.values());
+//        return "cheese/edit";
+//    }
+
     @RequestMapping(value = "", method = RequestMethod.GET)
-    public String index(Model model, int id) {
+    public String index(Model model, @RequestParam("id") int jobID) {
 
         // TODO #1 - get the Job with the given ID and pass it into the view
-
+        Job jobByID = jobData.findById(jobID);
+        //
+        model.addAttribute("job", jobByID);
         return "job-detail";
     }
 
